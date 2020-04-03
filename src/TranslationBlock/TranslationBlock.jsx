d3f942191f5a14cc7714dfc618c6ffcb0afdd9f1
Veronica Andrade <veronica726@gmail.com>
2020-04-03 11:47:10 -0600

    Add base TranslationBlock component/styles files

---
+++ src/TranslationBlock/TranslationBlock.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TranslationBlock.scss';
+
+const TranslationBlock = () => {
+  return (
+    <section className='translation-block'>
+      <section className='specified-lang-sect'>
+
+      </section>
+    </section>
+  )
+}
+
+export default TranslationBlock;
