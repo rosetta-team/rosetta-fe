94cc6d02b00d1deb6bdfb4e684a4ca5b6c5ad21c
Veronica Andrade <veronica726@gmail.com>
2020-04-04 15:41:52 -0600

    Add jsx tags for designated lang

--- src/TranslationBlock/TranslationBlock.jsx
+++ src/TranslationBlock/TranslationBlock.jsx
@@ -5,7 +5,22 @@ const TranslationBlock = () => {
   return (
     <section className='translation-block'>
       <section className='specified-lang-sect'>
-
+        <section className='source-lang-name'>
+          <h2>Translate from:</h2>
+          <select>
+            <option value='select language'>select language</option>
+            <option>Ruby</option>
+            <option>JavaScript</option>
+          </select>
+        </section>
+        <section className='target-lang-name'>
+          <h2>Translate to:</h2>
+          <select>
+            <option value='select language'>select language</option>
+            <option>Ruby</option>
+            <option>JavaScript</option>
+          </select>
+        </section>
       </section>
     </section>
   )
