c5d336f4e1f0f021c9338c66754c6b11a78f8c88
Veronica Andrade <veronica726@gmail.com>
2020-04-06 12:36:51 -0600

    Import apollo client, apollo provider, and gql as well as include example query to test that it was installed correctly

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App.jsx';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+// or you can use `import gql from 'graphql-tag';` instead
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
+//doing a quick query here to make sure it was installed correctly 
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
