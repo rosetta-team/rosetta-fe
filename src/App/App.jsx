db7aab80c3146a6cf57461e1ea155e2aa9f5daad
Veronica Andrade <veronica726@gmail.com>
2020-04-09 15:45:04 -0600

    Add content to components

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React,{ Component }from 'react';
 import Navbar from '../Navbar/Navbar.jsx';
 import TranslationBlock from '../TranslationBlock/TranslationBlock';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
+import IntroCard from '../IntroCard/IntroCard'
 import './App.scss';
 
-function App() {
-  return (
-    <div className='main-app'>
-      <Navbar />
-      <TranslationBlock />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      results: []
+    }
+  }
+
+  render() {
+    return (
+      <div className='main-app'>
+        <section className='main-top'>
+          <Navbar />
+          <TranslationBlock />
+        </section>
+        <section className='main-results'>
+          {/* {this.state.results.length === 0 ? 
+          <IntroCard /> : */}
+          <ResultsContainer />
+        {/* } */}
+        </section>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/SourceBlock/SourceBlock.jsx
+++ src/SourceBlock/SourceBlock.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './SourceBlock.scss';
 
-const SourceBlock = () => {
+//Probably going to change the name of this component or completely delete it
+//What really needs to be in this section is the methods that 
+// populate the source lang methods and the translate button that 
+//Send the request to the backend. 
+
+const SourceBlock = ({ methods }) => {
   return (
     <section className='source-block-sect'>
-      <p>Choose a method you'd like to translate</p>
-      <select>
-        <option>.map</option>
-        <option>.split</option>
-        <option>.each</option>
-      </select>
+      <section className='method-dropdown-sect'>
+        <p className='method-label'>Select a method:</p>
+        <select className='select-method'>
+          <option>--method--</option>
+          {methods}
+        </select>
+      </section>
+      <section className='action-buttons-sect'>
+        <button className='reset-button'>Reset</button>
+        <button className='translate-button'>Translate</button>
+      </section>
     </section>
   )
 }
