9abb2fc92dab3930fd4f02c253746dc534c8b64a
Veronica Andrade <veronica726@gmail.com>
2020-04-12 12:05:24 -0600

    Set up basic actions that will be used to update the store

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+export const setSourceLanguage = (language) => ({
+  type: 'SET_SOURCE',
+  sourceLanguage: language
+})
+
+export const setSourceMethod = (method) => ({
+  type: 'SET_METHOD',
+  sourceMethod: method
+})
+
+export const setTargetLanguage = (language) => ({
+  type: 'SET_TARGET',
+  targetLanguage: language
+})
+
+export const setResults = (results) => ({
+  type: 'SET_RESULTS',
+  results
+})
