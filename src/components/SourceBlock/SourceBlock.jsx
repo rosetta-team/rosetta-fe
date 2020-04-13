2222a33704062bd4ad7f9b7fda975b954c1873cc
Veronica Andrade <veronica726@gmail.com>
2020-04-13 17:39:36 -0600

    Connect SourceBlock component with redux and pass methods for events

--- src/components/SourceBlock/SourceBlock.jsx
+++ src/components/SourceBlock/SourceBlock.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import '../../styles/SourceBlock.scss';
 
-const SourceBlock = ({ methods }) => {
+const SourceBlock = ({ methods, handleSubmit, handleMethodChange }, props) => {
 let sourceMethods = methods.map(method => <option key={method.id} id={method.id}>{method.name}</option>)
-  return (
-    <section className='source-block-sect'>
+return (
+  <section className='source-block-sect'>
       <section className='method-dropdown-sect'>
         <p className='method-label'>Select a method:</p>
-        <select className='select-method'>
+        <select className='select-method' onChange={(event) => handleMethodChange(event)}>
           <option>--method--</option>
           {sourceMethods}
         </select>
       </section>
       <section className='action-buttons-sect'>
         <button className='reset-button'>Reset</button>
-        <button className='translate-button'>Translate</button>
+        <button className='translate-button' onClick={() => handleSubmit()}>Translate</button>
       </section>
     </section>
   )
 }
 
-export default SourceBlock;
+export const mapStateToProps = state => ({
+  targetLanguage: state.targetLanguage,
+  sourceMethods: state.sourceMethod
+})
+
+export default connect(mapStateToProps)(SourceBlock);
