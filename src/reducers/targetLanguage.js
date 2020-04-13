fd13b9475fab6119aa633f6b8bc9fc5f67e4a8c7
Veronica Andrade <veronica726@gmail.com>
2020-04-13 17:42:48 -0600

    Add to target Language reducer

--- src/reducers/targetLanguage.js
+++ src/reducers/targetLanguage.js
@@ -1,7 +1,9 @@
-export const targetLanguage = (state = '', action) => {
+export const targetLanguage = (state = {}, action) => {
   switch (action.type) {
     case 'SET_TARGET':
-      return action.targetLanguage
+      return {...state, name:action.targetLanguage}
+    case 'SET_TARGET_ID':
+      return {...state, id:action.targetId}
     default:
       return state;
   }
