39b20513b4c50f5cdf0c99ec6d3c45f112c821ed
Veronica Andrade <veronica726@gmail.com>
2020-04-12 20:29:11 -0600

    Move apollo provider wrapper around app in index

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,27 +2,24 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar.jsx';
 import TranslationBlock from '../TranslationBlock/TranslationBlock';
 import ResultsContainer from '../../containers/ResultsContainer';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
 import '../../styles/App.scss';
+import { getAllLanguages } from '../../queries'
+import { useQuery } from "@apollo/react-hooks";
 
-const client = new ApolloClient({
-  uri: 'https://rosetta-server.herokuapp.com/graphql',
-});
+
 
 const App = () => {
+  let { data, loading, error } = useQuery(getAllLanguages)
   return (
-    <ApolloProvider client={client}>
-      <div className='main-app'>
-        <section className='main-top'>
-          <Navbar />
-          <TranslationBlock />
-        </section>
-        <section className='main-results'>
-          <ResultsContainer />
-        </section>
-      </div>
-    </ApolloProvider>
+    <div className='main-app'>
+      <section className='main-top'>
+        <Navbar />
+        <TranslationBlock />
+      </section>
+      <section className='main-results'>
+        <ResultsContainer />
+      </section>
+    </div>
   )
 }
 
