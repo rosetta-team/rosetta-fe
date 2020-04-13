0ff4b68e0251cb7df7e3b1f4c844b45bb16c98ec
Veronica Andrade <veronica726@gmail.com>
2020-04-13 17:39:04 -0600

    Remove query

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,8 @@ import Navbar from '../Navbar/Navbar.jsx';
 import TranslationBlock from '../TranslationBlock/TranslationBlock';
 import ResultsContainer from '../../containers/ResultsContainer';
 import '../../styles/App.scss';
-import { getAllLanguages } from '../../queries'
-import { useQuery } from "@apollo/react-hooks";
-
-
 
 const App = () => {
-  let { data, loading, error } = useQuery(getAllLanguages)
   return (
     <div className='main-app'>
       <section className='main-top'>
