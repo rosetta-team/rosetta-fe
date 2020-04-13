6ffb57c8b3d330fa13646c798e40d93894115e5f
Veronica Andrade <veronica726@gmail.com>
2020-04-13 17:33:30 -0600

    Set up all actions needed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,17 @@
 export const setSourceLanguage = (language) => ({
   type: 'SET_SOURCE',
-  sourceLanguage: language
+  sourceLanguage: language,
 })
 
-export const setSourceMethod = (method) => ({
+export const setSourceId = (id) => ({
+  type: 'SET_SOURCE_ID',
+  sourceId: id
+})
+
+export const setSourceMethod = (method, id) => ({
   type: 'SET_METHOD',
-  sourceMethod: method
+  sourceMethod: method,
+  methodId: id
 })
 
 export const setTargetLanguage = (language) => ({
@@ -13,12 +19,12 @@ export const setTargetLanguage = (language) => ({
   targetLanguage: language
 })
 
+export const setTargetId = (id) => ({
+  type: 'SET_TARGET_ID',
+  targetId: id
+})
+
 export const setResults = (results) => ({
   type: 'SET_RESULTS',
   results
-})
-
-export const setLanguages = (languages) => ({
-  type: 'SET_LANGUAGES',
-  languages
 })
