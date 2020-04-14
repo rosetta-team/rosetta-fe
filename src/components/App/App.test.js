b9a4f6af8118a86b6c77439cee9b3775172048c2
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-04-14 12:19:11 -0600

    Refactor test of App component to render with necessary providers

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import App from './App';
+import { render } from '@testing-library/react'
 
-describe('App', () => {
-  it('should pass first test', () => {
-    expect(true).toEqual(true);
+import { createStore } from 'redux';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { Provider } from 'react-redux';
+import { rootReducer } from '../../reducers';
+
+describe('App component', () => {
+  it('can render', () => {
+    const client = new ApolloClient({ uri: 'fake.com' });
+    const store = createStore(rootReducer);
+    const { getByText } = render(<ApolloProvider client={client}><Provider store={store}><App /></Provider></ApolloProvider>);
+    // const { getByText } = render(<App />);
+    // expect(getByText(mockMethod.name)).toBeInTheDocument();
+    // expect(true).toEqual(true);
   })
 })
