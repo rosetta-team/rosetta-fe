a04eb39d7a6c94c7500ce595eaf4aea6e431d129
Veronica Andrade <veronica726@gmail.com>
2020-04-14 10:29:11 -0600

    Add SourceMethod component to results container

--- src/containers/ResultsContainer.jsx
+++ src/containers/ResultsContainer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import IntroCard from '../components/IntroCard/IntroCard'
 import '../styles/ResultsContainer.scss';
-import { connect } from 'react-redux'
-import ResultCard from '../components/ResultCard/ResultCard'
+import { connect } from 'react-redux';
+import SourceMethod from '../components/SourceMethod/SourceMethod';
+import ResultCard from '../components/ResultCard/ResultCard.jsx'
 
 const ResultsContainer = (props) => {
   let results = props.results.map(result => {
@@ -11,9 +12,14 @@ const ResultsContainer = (props) => {
 
   return (
     <section className='results-container'>
-      <article className='results-header'>
-        <h2 className='results-title'>Results:</h2>
-      </article>
+      <section className='source-section'> 
+        <article className='results-header'>
+          <h2 className='results-title'>Results:</h2>
+        </article>
+          <section className='source-method-section'>
+            <SourceMethod />
+          </section>
+      </section>
       <section className='results-sect'>
       {props.results?
         results:null
