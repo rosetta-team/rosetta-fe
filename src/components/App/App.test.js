84b4114b8e75fa1ea6832848c4a3aebfafc4913c
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-04-14 12:53:27 -0600

    Add assertion to test of App component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import App from './App';
 import { render } from '@testing-library/react'
-
 import { createStore } from 'redux';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
@@ -13,8 +12,6 @@ describe('App component', () => {
     const client = new ApolloClient({ uri: 'fake.com' });
     const store = createStore(rootReducer);
     const { getByText } = render(<ApolloProvider client={client}><Provider store={store}><App /></Provider></ApolloProvider>);
-    // const { getByText } = render(<App />);
-    // expect(getByText(mockMethod.name)).toBeInTheDocument();
-    // expect(true).toEqual(true);
+    expect(getByText('Rosetta')).toBeInTheDocument();
   })
 })
