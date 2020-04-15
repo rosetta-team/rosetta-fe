5ac69035886cfd5ae755d34d1cb39b87a71f7800
Veronica Andrade <veronica726@gmail.com>
2020-04-15 11:04:59 -0600

    Reset source method

--- src/reducers/sourceMethod.js
+++ src/reducers/sourceMethod.js
@@ -1,7 +1,9 @@
 export const sourceMethod = (state = {}, action) => {
   switch (action.type) {
     case 'SET_METHOD':
-      return {...state, action.sourceMethod}
+      return {...state, ...action.sourceMethod}
+    case 'RESET_SOURCE_METHOD':
+      return {}
     default:
       return state;
   }
