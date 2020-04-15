424ae36f6d5e91eca0cd1ba922bd2bb1eb3faab1
Veronica Andrade <veronica726@gmail.com>
2020-04-15 11:02:26 -0600

    Add error handling

--- src/components/SourceBlock/SourceBlock.jsx
+++ src/components/SourceBlock/SourceBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import '../../styles/SourceBlock.scss';
 
-const SourceBlock = ({ methods, handleSubmit, handleMethodChange }, props) => {
+const SourceBlock = ({ methods, handleSubmit, handleMethodChange, error, clearDropBoxes }, props) => {
 let sourceMethods = methods.map(method => <option key={method.id} id={method.id}>{method.name}</option>)
 return (
   <section className='source-block-sect'>
@@ -13,8 +13,13 @@ return (
           {sourceMethods}
         </select>
       </section>
+      { error ?
+      <section className='input-error'>
+        <p className='error-message'>**You must fill out all inputs**</p>
+      </section> : null
+      }
       <section className='action-buttons-sect'>
-        <button className='reset-button'>Reset</button>
+        <button className='reset-button' onClick={() => clearDropBoxes()}>Reset</button>
         <button className='translate-button' onClick={() => handleSubmit()}>Translate</button>
       </section>
     </section>
@@ -23,7 +28,8 @@ return (
 
 export const mapStateToProps = state => ({
   targetLanguage: state.targetLanguage,
-  sourceMethods: state.sourceMethod
+  sourceMethods: state.sourceMethod,
+  sourceLanguage: state.sourceLanguage
 })
 
 export default connect(mapStateToProps)(SourceBlock);
